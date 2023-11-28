e9c31caeeb3006f5648f1f1c1ffc9601f2932fcd
MOHAMMED-BOUAZZA <mohammed.bouazza1994@gmail.com>
2023-11-27 16:11:20 -0800

    Python - Hello, World

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
+#!#!/usr/bin/python3
 number = 98
-print("{:d} Battery street".format(number))
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
+#!/usr/bi#!/usr/bin/python3
 number = 3.14159
-print("Float: {:.2f}".format(number))
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
