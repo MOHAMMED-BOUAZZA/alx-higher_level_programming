be0c210fcf1d1bca2bbb8c3b63575db45450f39c
MOHAMMED-BOUAZZA <mohammed.bouazza1994@gmail.com>
2023-12-03 17:28:26 -0800

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    my_tuple = ()
     if len(sentence) == 0:
-        my_tuple = (0, None)
+        my_tuple = 0, "None"
     else:
-        my_tuple = (len(sentence), sentence[:1])
-    return(my_tuple)
+        my_tuple = len(sentence), sentence[0]
+    return my_tuple
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == []:
-        return(None)
-    my_list.sort()
-    return(my_list[-1])
+    if len(my_list) == 0:
+        return "None"
+    else:
+        max = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > max:
+                max = my_list[i]
+        return max
