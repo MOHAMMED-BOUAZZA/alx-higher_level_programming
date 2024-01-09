e9475ce632321443f768649435dc51a886a0e2bf
MOHAMMED-BOUAZZA <mohammed.bouazza1994@gmail.com>
2024-01-09 09:23:02 -0800

    Python - Input/Output

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-'''append_after'''
+"""Defines a text file insertion function."""
 
 
 def append_after(filename="", search_string="", new_string=""):
-	'''search and update'''
-	read = []
-	with open(filename, "r", encoding="utf-8") as f:
-		read = f.readlines()
-		index = 0
+    """Insert text after each line containing a given string in a file.
 
-		while index < len(read):
-			if search_string in read[index]:
-				read[index:index + 1] = [read[index], new_string]
-				index += 1
-			index += 1
-
-	with open(filename, "w", encoding="utf-8") as file:
-		file.writelines(read)
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search for within the file.
+        new_string (str): The string to insert.
+    """
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
