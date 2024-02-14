e2f38272a7020788409e9fe64588abbd99bf05ab
MOHAMMED-BOUAZZA <mohammed.bouazza1994@gmail.com>
2024-02-14 00:40:17 -0800

    SQL - Introduction

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,4 @@
--- Insert values
--- Execute: cat 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+-- inserts a new row in the table first_table, database hbtn_0c_0
+
+-- insert the row
+INSERT INTO first_table (id, name) VALUES (89, "Holberton School");
