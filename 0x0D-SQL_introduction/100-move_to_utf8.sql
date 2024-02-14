af74e82aeac3b5b3a60cb4aa667fae9aa98b2e7d
MOHAMMED-BOUAZZA <mohammed.bouazza1994@gmail.com>
2024-02-14 00:45:31 -0800

    SQL - Introduction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,7 @@
--- Change character SET and COLLATE
--- cat 100-move_to_utf8.sql | mysql -hlocalhost -uroot -p
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-USE hbtn_0c_0
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CHANGE name name VARCHAR (256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Changes a property of a database in the server
+ALTER DATABASE hbtn_0c_0
+    CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Changes the properties of a table in the database
+USE hbtn_0c_0;
+ALTER TABLE first_table
+    CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
