a3cc1a97227f00519552f34cf121dbbcf2cbc44c
MOHAMMED-BOUAZZA <mohammed.bouazza1994@gmail.com>
2024-03-17 08:24:05 -0700

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
-# Creates the State “California” with the City “San Francisco”
-# from the database hbtn_0e_100_usa.
-# Usage: ./100-relationship_states_cities.py <mysql username> /
-#                                            <mysql password> /
-#                                            <database name>
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from relationship_state import State
-from relationship_city import Base, City
+"""script to create city and state object"""
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+        from sqlalchemy.engine import create_engine
+            from sqlalchemy.engine.url import URL
+                from sqlalchemy.orm import Session
+                    from relationship_city import City
+                        from relationship_state import Base, State
+                            from sys import argv
 
-    session.add(City(name="San Francisco", state=State(name="California")))
-    session.commit()
+                                db = {'drivername': 'mysql+mysqldb',
+                                                  'host': 'localhost',
+                                                            'port': '3306',
+                                                                      'username': argv[1],
+                                                                                'password': argv[2],
+                                                                                          'database': argv[3]}
+
+                                    url = URL(**db)
+                                        engine = create_engine(url, pool_pre_ping=True)
+                                            Base.metadata.create_all(engine)
+
+                                                session = Session(engine)
+                                                    city = City(name='San Francisco')
+                                                        state = State(name='California', cities=[city])
+
+                                                            session.add(state)
+                                                                session.add(city)
+                                                                    session.commit()
+                                                                        session.close())
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-# Defines a State model.
-# Inherits from SQLAlchemy Base and links to the MySQL table states.
+''' Defined State class which inherits from Base class '''
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from relationship_city import Base, City
+from sqlalchemy.orm import relationship
+
+# using declarative_base func to return a new base class, mapped class State
+# will inherit from Base and generate new Table and mapper()
+# declare new mapping
+Base = declarative_base()
 
 
+# map class to inherit from Base
 class State(Base):
-    """Represents a state for a MySQL database.
+    """mapped class definition"""
 
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store States.
-        id (sqlalchemy.Integer): The state's id.
-        name (sqlalchemy.String): The state's name.
-        cities (sqlalchemy.orm.relationship): The State-City relationship.
-    """
-    __tablename__ = "states"
+    # 3 important: Table, mapper(), class objects
+
+    # Table
+    __tablename__ = 'states'
+
+    # Describe table, column objects, use methods imported from sqlalchemy
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-
-    cities = relationship("City", backref="state", cascade="all, delete")
+    cities = relationship("City", cascade="all")
